c7716683c3f47e2316f389df118aa19d294ff69d
imn0thero <127270111+imn0thero@users.noreply.github.com>
2025-06-20 11:17:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 },
+  limits: { fileSize: 2 * 1024 * 1024 },
   fileFilter: function (req, file, cb) {
     const allowedTypes = /jpeg|jpg|png|gif|mp4|mov|avi|pdf|doc|docx|txt/;
     const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
